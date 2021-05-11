0c0f4d95c647845339fc3f1d5a458870d3e524a7
Artorys <77374081+Artorys@users.noreply.github.com>
2021-05-11 16:49:44 +0000

    Add files via upload

---
+++ condições.py
@@ -0,0 +1,11 @@
+import random
+sorte = random.choice([True, False])#tava aprendendo a usar o random kkkk
+print (sorte)
+
+
+lista_sabor = ['chocolate', 'baunilha', 'açaí','morango','leite-ninho']
+n = random.choice(lista_sabor)
+if n =='chocolate':
+    print('delicious')
+elif n == 'baunilha'or 'açaí' or 'morango' or 'leite-ninho':
+    print('eca')
